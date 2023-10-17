ebbbdf5ded85704854c4ce4f0ab5efc57378aade
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2023-10-17 04:12:15 +0200

    app.Get function implemented

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -21,10 +22,10 @@ const dbName = "fiber-hrms"
 const mongoURI = "mongodb://localhost:27017" + dbName
 
 type Employee struct {
-	ID     string
-	Name   string
-	Salary float64
-	Age    int
+	ID     string  `json:"id,omitempty" bson:"_id, omitempty"`
+	Name   string  `json:"name"`
+	Salary float64 `json:"salary"`
+	Age    int     `json:"age"`
 }
 
 // Helps to connect Golang with MongoDB
@@ -32,7 +33,7 @@ func Connect() error {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI)) // Using the driver to create a Client from a connection string
 	if err != nil {
 		return err
 	}
@@ -57,6 +58,21 @@ func main() {
 
 	// Get list of all employees in the database
 	app.Get("/employee", func(c *fiber.Ctx) error {
+
+		query := bson.D{}
+
+		cursor, err := mg.Db.Collection("employees").Find(c.Context(), query) // Use the collection to query the database (this method return a cursor)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+
+		var employees []Employee = make([]Employee, 0) // Create a slice to store the returned employees in the query
+
+		if err := cursor.All(c.Context(), &employees); err != nil { // Cursor.All will decode all of the returned elements at once
+			return c.Status(500).SendString(err.Error())
+		}
+
+		return c.JSON(employees)
 
 	})
 
